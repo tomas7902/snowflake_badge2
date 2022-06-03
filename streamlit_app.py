0414c553bde6225dae66d22ad6ca4cab51fe9e30
tomas7902 <106591505+tomas7902@users.noreply.github.com>
2022-06-03 10:39:09 +0200

    Select2

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,6 @@ my_cur.execute("Select Current_user(),current_account(),current_region()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+my_cur.execute("Select * from fruit_load_list")
+my_data_row = my_cur.fetchall()
