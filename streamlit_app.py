723238fe3a68d020f9fc8d2df7db81d4107718ea
tomas7902 <106591505+tomas7902@users.noreply.github.com>
2022-06-03 10:37:35 +0200

    Select from Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("Select Current_user(),current_account(),current_region()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
